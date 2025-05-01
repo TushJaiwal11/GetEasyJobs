ee9a36bc68d17ed04c4df1e4de706c01c41d69af
TushJaiwal11 <tusharjaiwal123@gmail.com>
2025-05-01 13:08:13 +0530

    changes in dash board

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div className="p-6 text-center text-xl">
-            Easy to Share Mail to HR With in few steps
+            Easy to Share Mail
         </div>
     );
 };
